a2e85e211cb7b2c7b13f78022ad7726a32b069b9
gabriel-waltmann <gabrielwaltmann@gmail.com>
2025-02-13 10:21:57 -0300

    db: add table factories

---
+++ database/factories/ContactAddressFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ContactAddress>
+ */
+class ContactAddressFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = ContactAddress::class;
+}
---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Contact;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Contact::class;
+}
